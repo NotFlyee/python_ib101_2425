8b3b4e1c1ed58f5bd5c1aaf59c0c2185a76d1c28
Flye <61046075+NotFlyee@users.noreply.github.com>
2025-03-27 09:09:43 +0500

    Update 01.py

--- 11/01.py
+++ 11/01.py
@@ -6,7 +6,7 @@ n = int(input())
 for i in range(len(data)):
     if answer:
         break
-    for j in range(len(data)):
+    for j in range(i + 1, len(data)):
         if data[i] == data[j]:
             continue
         if data[i] * data[j] == n:
