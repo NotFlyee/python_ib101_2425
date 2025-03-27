445de10af9a36cc403e13614037f39e80dd11675
Flye <61046075+NotFlyee@users.noreply.github.com>
2025-03-27 08:52:13 +0500

    Create 04.py

---
+++ 11/04.py
@@ -0,0 +1,11 @@
+data = []
+n = int(input())
+for i in range(n):
+    data.append(input())
+for i in range(n):
+    for j in range(i + 1, n):
+        if len(data[i]) > len(data[j]):
+            data[i], data[j] = data[j], data[i]
+for i in data:
+    print(i)
+
