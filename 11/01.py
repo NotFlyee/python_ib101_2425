9d7963526d33b2be76f323441af17e4a93de02cf
Flye <61046075+NotFlyee@users.noreply.github.com>
2025-03-27 08:29:33 +0500

    Create 01.py

---
+++ 11/01.py
@@ -0,0 +1,15 @@
+data = []
+answer = ''
+for i in range(int(input())):
+    data.append(int(input()))
+n = int(input())
+for i in range(len(data)):
+    if answer:
+        break
+    for j in range(len(data)):
+        if data[i] == data[j]:
+            continue
+        if data[i] * data[j] == n:
+            answer = 'ДА'
+            break
+print(answer) if answer else print('НЕТ')
