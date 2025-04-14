f18029a8290ca566cea0c8f12028699df3f5ab92
Flye <61046075+NotFlyee@users.noreply.github.com>
2025-04-14 08:51:07 +0500

    Update 05.py

--- 13/05.py
+++ 13/05.py
@@ -1,1 +1,1 @@
-print('\n'.join([line for line in [input().split('#')[0] for _ in range(int(input()[1:]))]]))
+print('\n'.join([line for line in [input().split('#')[0].rstrip() for _ in range(int(input()[1:]))]]))
