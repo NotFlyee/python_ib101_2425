16595a177802b5f7486bb644d6e156405d39a13d
Flye <61046075+NotFlyee@users.noreply.github.com>
2025-04-21 09:03:20 +0500

    Create 03.py

---
+++ 14/03.py
@@ -0,0 +1,41 @@
+text = list(input())
+chars = {
+    'а': 'a',
+    'б': 'b',
+    'в': 'v',
+    'г': 'g',
+    'д': 'd',
+    'е': 'e',
+    'ё': 'e',
+    'ж': 'zh',
+    'з': 'z',
+    'и': 'i',
+    'й': 'i',
+    'к': 'k',
+    'л': 'l',
+    'м': 'm',
+    'н': 'n',
+    'о': 'o',
+    'п': 'p',
+    'р': 'r',
+    'с': 's',
+    'т': 't',
+    'у': 'u',
+    'ф': 'f',
+    'х': 'kh',
+    'ц': 'tc',
+    'ч': 'ch',
+    'ш': 'sh',
+    'щ': 'shch',
+    'ъ': '',
+    'ы': 'y',
+    'ь': '',
+    'э': 'e',
+    'ю': 'iu',
+    'я': 'ia'
+}
+for i in range(len(text)):
+    char = text[i]
+    if char.lower() in chars:
+        text[i] = chars[char.lower()] if char.islower() else chars[char.lower()].capitalize()
+print(''.join(text))
