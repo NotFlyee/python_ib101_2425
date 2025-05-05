d8a12308398007bcc77c55b51ac32885478ca09a
Flye <61046075+NotFlyee@users.noreply.github.com>
2025-05-05 09:08:34 +0500

    Update 07.py

--- 15/07.py
+++ 15/07.py
@@ -5,7 +5,3 @@ def print_statistics(arr):
     print(min(arr) if arr else 0)
     print(max(arr) if arr else 0)
     print(int(temp[len(temp) // 2] if len(temp) % 2 else sum(temp[len(temp) // 2 - 1:len(temp) // 2 + 1]) / 2))
-
-
-print_statistics([])
-print_statistics([22])
