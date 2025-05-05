f7135e5acea00ef6d84d8d1cd4e64c6ef5dd6ad3
Flye <61046075+NotFlyee@users.noreply.github.com>
2025-05-05 08:49:07 +0500

    Create 04.py

---
+++ 15/04.py
@@ -0,0 +1,8 @@
+def who_are_you_and_hello():
+    name = input()
+    while len(name.split()) != 1 or 0 in [1 if 1040 <= ord(char) <= 1103 else 0 for char in name] or name != name.lower().capitalize():
+        name = input()
+    print(f'Привет, {name}')
+
+
+who_are_you_and_hello()
