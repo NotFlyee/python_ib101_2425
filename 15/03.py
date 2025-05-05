8c407d3e08d7b3842634b9da83bcc080a647dee9
Flye <61046075+NotFlyee@users.noreply.github.com>
2025-05-05 08:30:05 +0500

    Create 03.py

---
+++ 15/03.py
@@ -0,0 +1,13 @@
+def quarter(xcoord, ycoord):
+    if xcoord > 0 and ycoord > 0:
+        print('I четверть')
+    elif ycoord > 0:
+        print('II четверть')
+    elif xcoord < 0 and ycoord < 0:
+        print('III четверть')
+    else:
+        print('IV четверть')
+
+
+quarter(3, 4)
+quarter(-3.5, 8)
