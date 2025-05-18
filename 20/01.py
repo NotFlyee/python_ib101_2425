69a2860bf980e327d6c5a6eb4b699e0242370b62
Flye <61046075+NotFlyee@users.noreply.github.com>
2025-05-18 15:11:41 +0500

    Create 01.py

---
+++ 20/01.py
@@ -0,0 +1,8 @@
+def arithmetic_operation(operation):
+    operations = {
+        '+': lambda x, y: x + y,
+        '-': lambda x, y: x - y,
+        '*': lambda x, y: x * y,
+        '/': lambda x, y: x / y
+    }
+    return operations[operation]
