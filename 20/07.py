19eec121c324c06ad0c442896b350ac3fb4940ff
Flye <61046075+NotFlyee@users.noreply.github.com>
2025-05-18 18:27:55 +0500

    Update 07.py

--- 20/07.py
+++ 20/07.py
@@ -1,4 +1,4 @@
 def same_by(characteristic, objects: list[int]):
     if not objects:
         return True
-    return not (len(set(map(characteristic, objects))) - 1)
+    return len(set(map(characteristic, objects))) == 1
