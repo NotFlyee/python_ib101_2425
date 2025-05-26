66f08b3fbc0ba25498f462ab95c8473d7aff5fcd
Flye <61046075+NotFlyee@users.noreply.github.com>
2025-05-26 23:40:02 +0500

    Create 01.py

---
+++ 24/01.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageDraw
+
+def twist_image(input_ﬁle_name: str, output_ﬁle_name: str):
+    image = Image.open(input_file_name)
+    width, height = image.size
+    left_side, right_side = image.crop((0, 0, width * 0.5, height)), image.crop((width * 0.5, 0, width, height))
+    new_image = Image.new('RGB', (width, height))
+    draw = ImageDraw.Draw(new_image)
+
+    new_image.paste(right_side)
+    new_image.paste(left_side, (int(width * 0.5), 0))
+
+    new_image.save(output_file_name)
+
+
+twist_image('p_24_09.png', 'res.jpg')
