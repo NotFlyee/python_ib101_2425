82f2c3b73d1e6119ee474268220ecb4b9d12bf69
Flye <61046075+NotFlyee@users.noreply.github.com>
2025-05-27 09:09:08 +0500

    Update 05.py

--- 20/05.py
+++ 20/05.py
@@ -2,11 +2,12 @@ def main(login: str, password: str):
     ask_password(login, password, lambda login: print(f'Привет, {login}!'), lambda login, err: print(f'Кто-то пытался притвориться пользователем {login}, но в пароле допустил ошибку: {err.upper()}.'))
 
 def ask_password(login: str, password: str, success, failure):
-    to_vowels = lambda x, y: list(filter(lambda char: char in x, y))
-    to_consonants = lambda x, y: list(filter(lambda char: char not in x, y))
-    number_of_vowels = len(to_vowels('aeiouy', list(password.lower())))
-    consonants_in_password = to_consonants('aeiouy', list(password.lower())) 
-    consonants_in_login = to_consonants('aeiouy', list(login.lower()))
+    VOWELS = 'aeiouy'
+    to_vowels = lambda x: list(filter(lambda char: char in VOWELS, x))
+    to_consonants = lambda x: list(filter(lambda char: char not in VOWELS, x))
+    number_of_vowels = len(to_vowels(list(password.lower())))
+    consonants_in_password = to_consonants(list(password.lower())) 
+    consonants_in_login = to_consonants(list(login.lower()))
     if number_of_vowels != 3 and consonants_in_password != consonants_in_login:
         return failure(login, 'Everything is wrong')
     if number_of_vowels != 3:
